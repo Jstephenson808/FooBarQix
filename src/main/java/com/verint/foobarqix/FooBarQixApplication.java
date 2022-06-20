66fcfbbc9f1a80f7f59c0487c577fdfcb32b53e5
James Stephenson <2573758S@student.gla.ac.uk>
2022-06-20 14:52:47 +0100

    first commit

---
+++ src/main/java/com/verint/foobarqix/FooBarQixApplication.java
@@ -0,0 +1,13 @@
+package com.verint.foobarqix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooBarQixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooBarQixApplication.class, args);
+    }
+
+}
